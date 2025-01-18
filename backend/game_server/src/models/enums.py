ef902a16daea31d4bdf8adc63a57e4b2fb35fe62
maze508 <matthewmazexuan@gmail.com>
2025-01-18 20:50:06 +0800

    update bullet collision logic and cleanup

--- backend/game_server/src/models/enums.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class Position(Enum):
-    HELM = "helm"
-    CANNON_LEFT_1 = "cannon_left_1"
-    CANNON_LEFT_2 = "cannon_left_2"
-    CANNON_RIGHT_1 = "cannon_right_1"
-    CANNON_RIGHT_2 = "cannon_right_2"
